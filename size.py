36f4edf7c801de0416859576bc0c7619a2ff191b
Your Name <you@example.com>
2019-12-02 22:04:30 +0000

    no B in suffix

--- size.py
+++ size.py
@@ -3,8 +3,8 @@ import os
 import os.path
 import sys
 
-def sizeof_fmt(num, suffix='B'):
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+def sizeof_fmt(num, suffix=''):
+    for unit in ['','K','M','G','T','P','E','Z']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
