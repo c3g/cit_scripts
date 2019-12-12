69c9c6b8ca0e5cff48e28549c8bb59aba48aac45
P-O Quirion <pioliqui@gmail.com>
2019-12-12 10:21:13 -0500

    ad nb files in size.py

--- size.py
+++ size.py
@@ -12,11 +12,15 @@ def sizeof_fmt(num, suffix=''):
 
 
 tot = 0 
+nb = [0]
+def count(path):
+    nb[0] +=1 
+    return os.path.getsize(path)
 for root, dirs, files in os.walk(sys.argv[1]):
     paths = (os.path.join(root, f) for f in files)
-    tot += sum(os.path.getsize(path) for path in paths if  os.path.isfile(path))
+    tot += sum(count(path) for path in paths if  os.path.isfile(path))
     #for p in paths:
     #    print(p)
     #    print(os.path.getsize(p))
-print('{:15} for {:70}'.format(sizeof_fmt(tot),os.path.abspath(sys.argv[1])))             
+print('{0:>15} and {2:>12} files for {1:70}'.format(sizeof_fmt(tot), os.path.abspath(sys.argv[1]), nb[0]))             
 
