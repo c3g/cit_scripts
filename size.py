4ba27c442153ff1fe7c8ccb9697dd0df7be783ad
P-O Quirion <pioliqui@gmail.com>
2020-07-28 12:15:11 -0400

    cosmetic enhencement of size.py

--- size.py
+++ size.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
+import argparse
 import os
 import os.path
-import sys
 import time
+
 
 def sizeof_fmt(num, suffix=''):
     for unit in ['','K','M','G','T','P','E','Z']:
@@ -12,19 +13,38 @@ def sizeof_fmt(num, suffix=''):
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
 
-tot = 0 
-nb = [0]
-mod_time=[]
-def count(path):
-    nb[0] +=1 
-    mod_time.append(os.path.getmtime(path))
-    return os.path.getsize(path)
-for root, dirs, files in os.walk(sys.argv[1]):
-    paths = (os.path.join(root, f) for f in files)
-    tot += sum(count(path) for path in paths if  os.path.isfile(path))
-    #for p in paths:
-    #    print(p)
-    #    print(os.path.getsize(p))
-most_recent=time.ctime(max(mod_time))
-print('{0:>15}, last modif {3:>10}, {2:>12} files for {1:70}'.format(sizeof_fmt(tot), os.path.abspath(sys.argv[1]), nb[0], most_recent))             
+def main(directory=None):
+    """
+    :param directory: string, a folder on the system
+    :return: Tuple with (<total size of folder>, <the number of file in the folder>, <the modification time of the most
+    recent file>)
+    """
+
+    tot = 0
+    nb = [0]
+    mod_time = []
+    def count(path):
+        nb[0] += 1
+        mod_time.append(os.path.getmtime(path))
+        return os.path.getsize(path)
+    for root, dirs, files in os.walk(directory):
+        paths = (os.path.join(root, f) for f in files)
+        tot += sum(count(path) for path in paths if os.path.isfile(path))
+    most_recent = time.ctime(max(mod_time))
+
+    return (tot, nb[0], most_recent)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('directory', type=str)
+    parsed = parser.parse_args()
+
+    directory = parsed.directory
+    size_of_directory, nb_file, most_recent_file = main(directory=directory)
+
+    print('{0:>15}, last modif {3:>10}, {2:>12} files for {1:70}'.format(sizeof_fmt(size_of_directory),
+                                                                         os.path.abspath(directory),
+                                                                         nb_file,
+                                                                         most_recent_file))
 
