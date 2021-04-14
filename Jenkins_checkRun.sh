39f09ab873dec9fd7d91698591b99feaa42804d8
Paul Stretenowich <paul.stretenowich@gmail.com>
2021-04-14 15:17:01 -0400

    Adding script to check status of pipelines tested with cit

---
+++ Jenkins_checkRun.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+module load mugqic/python/3.7.3 mugqic/genpipes
+
+control_c() {
+  exit 0
+}
+
+BRANCH=dev
+
+if [ "$#" -ne 1 ] ; then 
+ echo "$0 <path of cit outputs>"
+ exit 1
+fi 
+
+RESULT_PATH=$1
+latest_dev=$(realpath  "${RESULT_PATH}")
+cd $latest_dev && mkdir -p cit_out && cd cit_out
+trap control_c SIGINT
+list=$(find ${latest_dev}  -maxdepth 3  -type d -name 'job_output' | xargs -L 1 -I@ sh -c "ls -t1 @/*job* | head -n 1 ")
+for jl in $list ; do
+  out=$( cut -d '/' -f 10 <<< "$jl" )
+  echo processing $out 
+  $MUGQIC_PIPELINES_HOME/utils/log_report.py  --loglevel CRITICAL -q --tsv $out.tsv $jl
+done 
+grep -v   "COMPLETED\+[[:space:]]\+COMPLETED\+[[:space:]]\+COMPLETED" *tsv | grep -v "log_from_job" | grep -v CANCELLED | sort -u -t: -k1,1
