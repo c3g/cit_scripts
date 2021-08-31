b9e62ae769b675f9b976cf3127b720c8ea623e77
P-O Quirion <pioliqui@gmail.com>
2021-08-31 16:01:08 -0400

    prevent deliting fresh run if ran on the last day of the month at midnight

--- ssh_util.sh
+++ ssh_util.sh
@@ -4,17 +4,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BRANCH=$1
 echo "running on **** $HOSTNAME ****"
 if [[ $1 -eq graham_full  ]]; then
-  $SCRIPT_DIR/integration_tests.sh -b dev -l
   cd /project/6002326/C3G/projects/jenkins_tests
   bash ./cleanup_old
+  $SCRIPT_DIR/integration_tests.sh -b dev -l
 
 elif [[ $1 -eq cedar_full  ]]; then
-  $SCRIPT_DIR/integration_tests.sh -b dev -l
   cd /project/6007512/C3G/projects/jenkins_tests
   bash ./cleanup_old
+  $SCRIPT_DIR/integration_tests.sh -b dev -l
 
 elif [ $# -eq beluga_full ]; then
-  $SCRIPT_DIR/integration_tests.sh -b ${BRANCH} -l
   cd /lustre03/project/6007512/C3G/projects/jenkins_tests
   bash  ./cleanup_old
+  $SCRIPT_DIR/integration_tests.sh -b ${BRANCH} -l
 fi
