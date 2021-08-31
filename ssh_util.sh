b7c8cfd96eb06424e9021c32ccfcaf534baae904
P-O Quirion <pioliqui@gmail.com>
2021-08-31 15:35:54 -0400

    update test on cedar/graham

--- ssh_util.sh
+++ ssh_util.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BRANCH=$1
 echo "running on **** $HOSTNAME ****"
 if [[ $1 -eq cedar_full || graham_full  ]]; then
-  $SCRIPT_DIR/integration_tests.sh -b dev -l -s
+  $SCRIPT_DIR/integration_tests.sh -b dev -l
 
 elif [ $# -eq beluga_full ]; then
   $SCRIPT_DIR/integration_tests.sh -b ${BRANCH} -l
