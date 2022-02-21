6b994ff6cc5fdd7b33a10fdc9f9d9b9911179a57
P-O Quirion <pioliqui@gmail.com>
2022-02-21 15:44:53 -0500

    update ssh_util

--- ssh_util.sh
+++ ssh_util.sh
@@ -4,19 +4,25 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BRANCH=$1
 echo "running on **** $HOSTNAME ****"
 ret_code=0
-if [[ $1 -eq graham_full  ]]; then
+if [[ $2 -eq graham_full  ]]; then
   cd /project/6002326/C3G/projects/jenkins_tests
   bash ./cleanup_old
-  $SCRIPT_DIR/integration_tests.sh -b dev -l
+  $SCRIPT_DIR/integration_tests.sh -b ${BRANCH} -l
   ret_code=$?
   scancel $USER
-elif [[ $1 -eq cedar_full  ]]; then
+elif [[ $2 -eq cedar_full  ]]; then
   cd /project/6007512/C3G/projects/jenkins_tests
   bash ./cleanup_old
-  $SCRIPT_DIR/integration_tests.sh -b dev -l
+  $SCRIPT_DIR/integration_tests.sh -b ${BRANCH} -l
   ret_code=$?
   scancel $USER
-elif [ $# -eq beluga_full ]; then
+elif [[ $2 -eq narval_full  ]]; then
+  cd /project/6007512/C3G/projects/jenkins_tests
+  bash ./cleanup_old
+  $SCRIPT_DIR/integration_tests.sh -b ${BRANCH}
+  ret_code=$?
+  scancel $USER
+elif [ $2 -eq beluga_full ]; then
   cd /lustre03/project/6007512/C3G/projects/jenkins_tests
   bash  ./cleanup_old
   $SCRIPT_DIR/integration_tests.sh -b ${BRANCH} -l
