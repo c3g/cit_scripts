a13a22206ae02f1cbe31bcb8789dfca79b46f788
P-O Quirion <pioliqui@gmail.com>
2022-02-21 16:33:30 -0500

    update run pr options

--- run_PR.sh
+++ run_PR.sh
@@ -2,11 +2,10 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 BRANCH=$1
-COMMIT=$2
+OPT=$2
 echo "running on **** $HOSTNAME ****"
 if [ $# -eq 1 ]; then
-  $SCRIPT_DIR/Jenkins_GenpipesFull.sh -b ${BRANCH}
-
-elif [ $# -eq 2 ]; then
-  $SCRIPT_DIR/Jenkins_GenpipesFull.sh -b ${BRANCH} -c ${COMMIT}
+  $SCRIPT_DIR/integration_tests.sh -b ${BRANCH}
+else [ $# -eq 2 ]; then
+  $SCRIPT_DIR/integration_tests.sh -b ${BRANCH} ${OPT}
 fi
