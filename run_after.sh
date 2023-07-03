2c1d355b1e784257b0ef37890da0464a6793f4a5
Mareike Janiak <mareike.janiak@computationalgenomics.ca>
2023-07-03 14:11:52 -0400

    run_after.sh : change grep cmd to include TIMEOUTs in digest.log

--- run_after.sh
+++ run_after.sh
@@ -76,7 +76,7 @@ done
 
 echo "########################################################" > digest.log
 grep -v   "COMPLETED\+[[:space:]]\+COMPLETED\+[[:space:]]\+COMPLETED" *tsv \
-| grep -v "log_from_job" | grep -v CANCELLED >> digest.log
+| grep -v "log_from_job" | grep -e 'TIMEOUT' -e 'FAILED' >> digest.log
 echo "########################################################" >> digest.log
 cat \${SLURM_SUBMIT_DIR}/log_report.log >> digest.log
 ${SEND_TO_J}
