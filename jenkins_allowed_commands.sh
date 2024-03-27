c2b146b6cc1b884b6e66df13f8805123abda7fd3
Paul Stretenowich <paul.stretenowich@mcgill.ca>
2024-03-27 10:48:56 -0400

    Debug

--- jenkins_allowed_commands.sh
+++ jenkins_allowed_commands.sh
@@ -8,6 +8,34 @@ function reject_command() {
 
 }
 
+function disk_space_monitor() {
+    bash Jenkins_diskSpaceMonitor.sh
+}
+
+function disk_space_monitor_mini() {
+    cd cit_scripts && git checkout master && git pull && bash Jenkins_diskSpaceMonitorMini.sh
+}
+
+function genpipes_full() {
+    cd cit_scripts && git checkout master && git pull && bash ssh_util.sh "$(${SSH_ORIGINAL_COMMAND#*genpipes_full })"
+}
+
+function genpipes_command() {
+    cd cit_scripts && git checkout master && git pull && bash Jenkins_GenpipesCommands.sh
+}
+
+function update_cache() {
+    /lustre03/project/6002326/poq/lmod_caching/update_cache.sh
+}
+
+function moh_genpipes() {
+    cd moh_automation && git checkout main && git pull && bash jenkins_genpipes.sh "$(${SSH_ORIGINAL_COMMAND#*moh_genpipes } | tr -d '\"')"
+}
+
+function moh_wrapper() {
+    cd moh_automation && git checkout main && git pull && bash jenkins_wrapper.sh
+}
+
 logger -t automation -p local0.info "Command called by $THIS_SCRIPT for user $USER: $SSH_ORIGINAL_COMMAND"
 
 case "$SSH_ORIGINAL_COMMAND" in
@@ -36,31 +64,3 @@ case "$SSH_ORIGINAL_COMMAND" in
         reject_command
         ;;
 esac
-
-function disk_space_monitor() {
-    bash Jenkins_diskSpaceMonitor.sh
-}
-
-function disk_space_monitor_mini() {
-    cd cit_scripts && git checkout master && git pull && bash Jenkins_diskSpaceMonitorMini.sh
-}
-
-function genpipes_full() {
-    cd cit_scripts && git checkout master && git pull && bash ssh_util.sh "$(${SSH_ORIGINAL_COMMAND#*genpipes_full })"
-}
-
-function genpipes_command() {
-    cd cit_scripts && git checkout master && git pull && bash Jenkins_GenpipesCommands.sh
-}
-
-function update_cache() {
-    /lustre03/project/6002326/poq/lmod_caching/update_cache.sh
-}
-
-function moh_genpipes() {
-    cd moh_automation && git checkout main && git pull && bash jenkins_genpipes.sh "$(${SSH_ORIGINAL_COMMAND#*moh_genpipes } | tr -d '\"')"
-}
-
-function moh_wrapper() {
-    cd moh_automation && git checkout main && git pull && bash jenkins_wrapper.sh
-}
